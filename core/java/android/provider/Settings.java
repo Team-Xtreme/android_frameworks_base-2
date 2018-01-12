8a13c7de8a7a5e7bc8d481e9bc830fca61b7f3bf
bigrushdog <randall.rushing@gmail.com>
2018-01-12 12:35:28 -0800

    Screen off animations [1/2]
    Fade (default), CRT, and Scale
    
    Credit and respect to @xplodwild for paving the way
    back in the KitKat days!
    
    History of the ElectronBeam class can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/kitkat/services/java/com/android/server/power/ElectronBeam.java
    
    Adapted to 10 by @eyosen
    
    Change-Id: I58d269d44c901a8c0471807e3cf05c2054205d28

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5613,6 +5613,15 @@ public final class Settings {
         public static final String AMBIENT_VISUALIZER_ENABLED = "ambient_visualizer_enabled";
 
         /**
+         * Defines the screen-off animation to display
+         * @hide
+         */
+        public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+        /** @hide */
+        private static final Validator SCREEN_OFF_ANIMATION_VALIDATOR = ANY_INTEGER_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5748,6 +5757,7 @@ public final class Settings {
             SWAP_VOLUME_BUTTONS,
             ALLOW_INCALL_HOME,
             LOCKSCREEN_MEDIA_BLUR,
+            SCREEN_OFF_ANIMATION,
         };
 
         /**
@@ -5944,6 +5954,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ALLOW_INCALL_HOME);
             PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_BLUR);
             PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_METADATA);
+            PRIVATE_SETTINGS.add(SCREEN_OFF_ANIMATION);
         }
 
 
@@ -6117,6 +6128,7 @@ public final class Settings {
             VALIDATORS.put(ALLOW_INCALL_HOME, ALLOW_INCALL_HOME_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_MEDIA_BLUR, LOCKSCREEN_MEDIA_BLUR_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_MEDIA_METADATA, LOCKSCREEN_MEDIA_METADATA_VALIDATOR);
+            VALIDATORS.put(SCREEN_OFF_ANIMATION, SCREEN_OFF_ANIMATION_VALIDATOR);
         }
 
         /**
