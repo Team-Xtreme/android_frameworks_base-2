Evolution-X-AOSP/frameworks_base

base: Allow disabling of screenshot shutter sound [1/2]
Change-Id: I47d52bba21170118af87d35376d81d7569587a2f

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -67,6 +67,7 @@ import android.os.PowerManager;
 import android.os.Process;
 import android.os.UserHandle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Slog;
@@ -762,8 +763,11 @@ class GlobalScreenshot {
         mScreenshotLayout.post(new Runnable() {
             @Override
             public void run() {
-                // Play the shutter sound to notify that we've taken a screenshot
-                mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
+                    // Play the shutter sound to notify that we've taken a screenshot
+                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                }
 
                 mScreenshotView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotView.buildLayer();

Team-Xtreme/android_frameworks_base-2

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -766,10 +766,9 @@ class GlobalScreenshot {
             @Override
             public void run() {
                 // Play the shutter sound to notify that we've taken a screenshot
-                if (Settings.System.getInt(mContext.getContentResolver(), Settings.System.SCREENSHOT_SOUND, 1) == 1) {
-                    if (mScreenshotSound != null) {
-                        mScreenshotSound.play();
-                    }
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.SCREENSHOT_SOUND, 0) == 1) {
+                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
                 }
 
                 mScreenshotView.setLayerType(View.LAYER_TYPE_HARDWARE, null);